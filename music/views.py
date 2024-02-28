62e62d23e9b27c0d84fab6568f35d8bf963c8edf
samuelmideksa <113627346+samuelmideksa@users.noreply.github.com>
2024-02-28 16:21:42 +0300

    Home page changed to show fetured reviews

--- music/views.py
+++ music/views.py
@@ -1,12 +1,30 @@
 from django.contrib.sites import requests
 from django.shortcuts import render
-from .models import Album, News, UserMessage
+from .models import Album, News, UserMessage, Review
+from django.db.models import Exists, OuterRef, Subquery
 
 
 def home(request):
-    albums = Album.objects.all()
+    # Query to fetch albums with reviews
+    albums_with_reviews = Album.objects.filter(
+        Exists(Review.objects.filter(album=OuterRef('pk')))
+    ).annotate(
+        latest_review=Subquery(
+            Review.objects.filter(album=OuterRef('pk')).order_by('-published_date').values('review')[:1]
+        ),
+        reviewer=Subquery(
+            Review.objects.filter(album=OuterRef('pk')).order_by('-published_date').values('user')[:1]
+        ),
+        reviewer_rating=Subquery(
+            Review.objects.filter(album=OuterRef('pk')).order_by('-published_date').values('rating')[:1]
+        ),
+        review_published_date=Subquery(
+            Review.objects.filter(album=OuterRef('pk')).order_by('-published_date').values('published_date')[:1]
+        ),
+    )
+
     context = {
-        'albums': albums,
+        'albums': albums_with_reviews,
     }
     return render(request, 'home.html', context)
 
